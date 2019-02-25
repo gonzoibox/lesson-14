37ab6aff968a3b5e9b6600ec4ddd32fd2d36b380
anton_davidovich <dvd14@ukr.net>
2019-02-25 20:47:53 +0200

    make a form

--- express-app/routes/index.js
+++ express-app/routes/index.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/search', function(req, res, next) {
+  const {age, gender} = req.query;
+  console.log(age, gender);
+  res.render('index', { title: 'Express', age: 'Age', gender: 'Gender' });
 });
+
+router.post('/test', function(req, res, next){
+  console.log(req.body.txt); 
+  res.send(`Data ${req.body.txt} was sent`);
+})
 
 module.exports = router;
