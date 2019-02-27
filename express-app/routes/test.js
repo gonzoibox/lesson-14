8d7213af585aa850e7290a65672fe02dd2bd2e44
anton_davidovich <dvd14@ukr.net>
2019-02-27 22:16:15 +0200

    fix

--- express-app/routes/test.js
+++ express-app/routes/test.js
@@ -3,16 +3,20 @@ var router = express.Router();
 
 /* GET test page. */
 router.all('/', function(req, res, next) {
-    res.render('test', {text: req.body.txt});
+  let text = req.body.txt;
+    res.render('test', { text: text});
     next();
   });
 
 router.get('/', function(req, res, next) {
-  res.render('test');
+
+  res.render('test', { text: text});
 });
 
 router.post('/', function(req, res, next){
   console.log(req.body.txt); 
+  
+  req.body.txt;
   //res.send(`Data ${req.body.txt} was sent`);
 })
 
