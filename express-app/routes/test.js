5fff7fe60d2e50290ae42f0af050524ad977fdd4
anton_davidovich <dvd14@ukr.net>
2019-02-27 22:19:38 +0200

    Fix formating

--- express-app/routes/test.js
+++ express-app/routes/test.js
@@ -2,20 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET test page. */
-router.all('/', function(req, res, next) {
+router.all('/', function (req, res, next) {
   let text = req.body.txt;
-    res.render('test', { text: text});
-    next();
+  res.render('test', {
+    text: text
   });
-
-router.get('/', function(req, res, next) {
-
-  res.render('test', { text: text});
+  next();
 });
 
-router.post('/', function(req, res, next){
-  console.log(req.body.txt); 
-  
+router.get('/', function (req, res, next) {
+
+  res.render('test', {
+    text: text
+  });
+});
+
+router.post('/', function (req, res, next) {
+  console.log(req.body.txt);
+
   req.body.txt;
   //res.send(`Data ${req.body.txt} was sent`);
 })
